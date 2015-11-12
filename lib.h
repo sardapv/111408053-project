583c0bf736b7c7c6e73e99acc18336b5e14513c3
sardapv <sardapranav1@gmail.com>
2015-11-12 17:04:55 +0000

    Update lib.h

--- lib.h
+++ lib.h
@@ -5,18 +5,21 @@ struct lib {
 	char writer[32];
 	char category[32];
 	int book_no;
-	int issueddd, issuedmm, issuedyy;
-	int duedd, duemm, dueyy;
+	int dd, mm, yy;
 }lib;
-void Password();
-void display_book();/*display book details*/
+
+void Password();/*Password protection*/
+
+void display_book(struct lib);/*display book details*/
 
 void addbook();/*add book information*/
 
-void delete_book();/*delete  book info/records by user given key eg.authorname, book no, */
+void delete_book(struct lib);/*delete  book info/records on by user given key eg.authorname, book no, */
 
-void search();/*search  book info/records by user given key eg.authorname, book no, */
+void search(struct lib);/*search  book info/records by user given key eg.authorname, book no, */
 
-void edit_info();/*edit  book info/records by user given key eg.authorname, book no, */
+void edit_info(struct lib);/*edit  book info/records by user given key eg.authorname, book no, */
 
 void shortinfo(struct lib); /*short information on what added*/
+
+int checkdate(int day, int month, int year);
