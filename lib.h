70a080ea733939e578b0a0764f6ae5f8d60ebaf3
sardapv <sardapranav1@gmail.com>
2015-11-15 11:58:02 +0000

    Update lib.h

--- lib.h
+++ lib.h
@@ -8,6 +8,24 @@ struct lib {
 	int dd, mm, yy;
 }lib;
 
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m" 
+
 void Password();/*Password protection*/
 
 void display_book(struct lib);/*display book details*/
@@ -23,3 +41,5 @@ void edit_info(struct lib);/*edit  book info/records by user given key eg.author
 void shortinfo(struct lib); /*short information on what added*/
 
 int checkdate(int day, int month, int year);
+
+void exitp();
